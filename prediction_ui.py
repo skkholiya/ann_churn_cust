2f0b59041c6026b8afcf29a3a8988fb9494e4dde
skkholiya <33011852+skkholiya@users.noreply.github.com>
2025-10-26 11:29:57 +0530

    Add files via upload

---
+++ prediction_ui.py
@@ -0,0 +1,67 @@
+import pickle
+import streamlit as st
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
+
+path_prefix = '/Users/sureshkholiya/Desktop/Code/pysnippet/dl/ann/'
+model = tf.keras.models.load_model(f'{path_prefix}models/model.h5')
+
+with open(f'{path_prefix}models/scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+with open(f'{path_prefix}models/onehot_encoder.pkl', 'rb') as f:
+    geo_encoder = pickle.load(f)
+
+with open(f'{path_prefix}models/label_encoder.pkl', 'rb') as f:
+    label_encoder = pickle.load(f)
+
+st.title('Customer Churn Prediction')
+
+#User Inputs
+geography = st.selectbox('Geography', geo_encoder.categories_[0])
+gender = st.selectbox('Gender',label_encoder.classes_)
+age = st.slider('Age', 18, 100)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+gender = label_encoder.transform([gender])[0]
+
+#Perpare input data for prediction
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Geography': [geography],
+    'Gender': [gender],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'EstimatedSalary': [estimated_salary]
+})
+
+#Encode categorical features
+geo_encoded = geo_encoder.transform(input_data[['Geography']]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=geo_encoder.get_feature_names_out(['Geography']))
+
+#Combine encoded features with input data
+input_data = pd.concat([input_data.drop(['Geography'],axis=1), geo_encoded_df], axis=1)
+scale_data = scaler.transform(input_data)
+
+#Prediction
+prediction = model.predict(scale_data)
+churn_probability = 1 / (1 + np.exp(-prediction[0][0]))
+#churn_probability = prediction[0][0]
+print("Churn Probability: ", scale_data,prediction,model.summary())
+
+if churn_probability > 0.5:
+    st.write(f'The customer is likely to churn with a probability of', churn_probability)
+else:
+    st.write(f'The customer is unlikely to churn with a probability of', churn_probability)
